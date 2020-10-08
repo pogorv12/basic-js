db132faf9ff28323714e6d9c12d4a286fa817ef1
Victor Pogorelov <pogorv@gmail.com>
2020-10-08 21:41:42 +0300

    feat: Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,11 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //In case of wrong input parameter type or inadequate activity value or absence of argument function must return false.
+
+  return Number(sampleActivity) > 0 && Number(sampleActivity) < MODERN_ACTIVITY && typeof(sampleActivity) === 'string' ? 
+              Math.ceil(Math.log(Number(sampleActivity)/MODERN_ACTIVITY) / (-0.693) * HALF_LIFE_PERIOD) : false;
+
+  
 };
