c77c2c26170dcf1bb6f74b73d68ddd8b2b750664
Victor Pogorelov <pogorv@gmail.com>
2020-11-06 00:08:49 +0100

    feat: VigenereCipheringMachine done

--- src/try.js
+++ src/try.js
@@ -1,26 +1,60 @@
-function func(str, options){
-
-        const repeatTimes = options.repeatTimes > 0 ? options.repeatTimes : 0;
-        const additionRepeatTimes = options.additionRepeatTimes > 0 ? options.additionRepeatTimes : 0;
-        const separator = options.separator ? options.separator : '+';
-        const additionSeparator = options.additionSeparator ? options.additionSeparator : '|';
-        const addition = typeof options.addition === 'undefined' ? '' : options.addition + '';
+class VigenereCipheringMachine {
+        isBackward = null;
         
-        let additionText = addition;
-
-        for (let i = 1; i < additionRepeatTimes; i++){
-                additionText += additionSeparator + addition;
+        constructor (direction){
+                this.isBackward = direction === false ? true : false;
         }
 
-        str = str + '' + additionText;
-        let repeatText = str;
+        encrypt(string, key) {
+                let theString = string.toLowerCase();
+                let theKey = key.toLowerCase();
+                let arrEn = [];
+                let indexString = 0;
+                let indexKey = 0;
+                
+                while (indexString < theString.length){
+                        let theStringCharCode = theString.charCodeAt(indexString),
+                                theKeyCharCode = theKey.charCodeAt(indexKey % theKey.length);
+                        
+                        if (theStringCharCode >= 97 && theStringCharCode <= 122) {
+                                arrEn.push(String.fromCharCode(65 + ((theStringCharCode + theKeyCharCode - 97 * 2) % 26)));
+                                indexKey ++;
+                        } else {
+                                arrEn.push(theString.charAt(indexString));  
+                        }
+                        indexString ++;
+                }
+                return isBackward ? arrEn.reverse().join('') : arrEn.join(''); 
 
-        for (let i = 1; i < repeatTimes; i++){
-                repeatText += separator + str;
+        }    
+        decrypt(string, key) {
+                let theString = string.toUpperCase();
+                let theKey = key.toUpperCase();
+                let arrEn = [];
+                let indexString = 0;
+                let indexKey = 0;
+
+                while (indexString < theString.length){
+                        let theStringCharCode = theString.charCodeAt(indexString),
+                                theKeyCharCode = theKey.charCodeAt(indexKey % theKey.length);
+                        
+                        if (theStringCharCode >= 65 && theStringCharCode <= 90) {
+                                arrEn.push(String.fromCharCode(65 + (theStringCharCode - theKeyCharCode >= 0 ? 
+                                                                        theStringCharCode - theKeyCharCode : 
+                                                                        26 + theStringCharCode - theKeyCharCode)));
+                                indexKey ++;
+                        } else {
+                                arrEn.push(theString.charAt(indexString));  
+                        }
+                        indexString ++;
+                }
+                return isBackward ? arrEn.reverse().join('') : arrEn.join('');  
         }
-        return repeatText;
-}
+      };
 
-console.log(func(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }));
-//nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null
-//nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null
+      const aaa = new VigenereCipheringMachine;
+
+      console.log (aaa.encrypt('attack at dawn!', 'alphonse'));
+      console.log (aaa.decrypt('AEIHQX SX DLLU!', 'alphonse'));
+
+      //65|90|97|122
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,57 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  isBackward = null;
+  
+  constructor (direction){
+          this.isBackward = direction === false ? true : false;
   }
-}
+
+  encrypt(string, key) {
+          let theString = string.toLowerCase();
+          let theKey = key.toLowerCase();
+          let arrEn = [];
+          let indexString = 0;
+          let indexKey = 0;
+          
+          while (indexString < theString.length){
+                  let theStringCharCode = theString.charCodeAt(indexString),
+                          theKeyCharCode = theKey.charCodeAt(indexKey % theKey.length);
+                  
+                  if (theStringCharCode >= 97 && theStringCharCode <= 122) {
+                          arrEn.push(String.fromCharCode(65 + ((theStringCharCode + theKeyCharCode - 97 * 2) % 26)));
+                          indexKey ++;
+                  } else {
+                          arrEn.push(theString.charAt(indexString));  
+                  }
+                  indexString ++;
+          }
+          return this.charCodeAtisBackward ? arrEn.reverse().join('') : arrEn.join(''); 
+
+  }    
+  decrypt(string, key) {
+          let theString = string.toUpperCase();
+          let theKey = key.toUpperCase();
+          let arrEn = [];
+          let indexString = 0;
+          let indexKey = 0;
+
+          while (indexString < theString.length){
+                  let theStringCharCode = theString.charCodeAt(indexString),
+                          theKeyCharCode = theKey.charCodeAt(indexKey % theKey.length);
+                  
+                  if (theStringCharCode >= 65 && theStringCharCode <= 90) {
+                          arrEn.push(String.fromCharCode(65 + (theStringCharCode - theKeyCharCode >= 0 ? 
+                                                                  theStringCharCode - theKeyCharCode : 
+                                                                  26 + theStringCharCode - theKeyCharCode)));
+                          indexKey ++;
+                  } else {
+                          arrEn.push(theString.charAt(indexString));  
+                  }
+                  indexString ++;
+          }
+          return this.isBackward ? arrEn.reverse().join('') : arrEn.join('');  
+  }
+};
 
 module.exports = VigenereCipheringMachine;
